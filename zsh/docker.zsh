8a9be3706e6ffc7eb4e7aca58255d320223788a8
Yosuke Kumakura <kumatch@gmail.com>
2018-02-21 11:13:10 +0900

    Added docker command aliases.

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,8 +1,11 @@
 # -*- mode: shell-script -*-
 
 #######################################################
-# docker, boot2docker
+# docker, docker-machine
 #######################################################
 alias d="docker"
 alias dm="docker-machine"
 alias dcom="docker-compose"
+
+alias dm_enable="eval \$(dm env)"
+alias dm_disable="eval \$(dm env -u)"
