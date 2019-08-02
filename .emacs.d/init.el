cb662bc3b195d7b97fb83ffdbe07cff32eba18be
Yosuke Kumakura <kumakura-yosuke@c-fo.com>
2019-08-02 10:18:16 +0900

    custom face setting.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,3 +19,23 @@
 ;; (setq install-elisp-repository-directory "~/.emacs.d/site-lisp/")
 
 (tool-bar-mode 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell insert-shebang yasnippet yaml-mode web-mode use-package textile-mode stylus-mode smex smartparens scss-mode projectile prodigy popwin php-mode pallet migemo markdown-mode json-mode jade-mode init-loader go-eldoc go-autocomplete fuzzy elscreen editorconfig dockerfile-mode color-moccur coffee-mode anzu ag ac-js2)))
+ '(rst-level-face-base-light 30))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rst-level-1-face ((t (:foreground "LightSkyBlue"))) t)
+ '(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
+ '(rst-level-3-face ((t (:foreground "Cyan1"))) t)
+ '(rst-level-4-face ((t (:foreground "chocolate1"))) t)
+ '(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
+ '(rst-level-6-face ((t (:foreground "Aquamarine"))) t))
