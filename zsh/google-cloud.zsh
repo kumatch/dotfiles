b59f8b6979ca1f5ae2df517c9c444bf4ca60f676
Yosuke Kumakura <kumatch@gmail.com>
2019-08-15 12:41:36 +0900

    added settings for google cloud.

---
+++ zsh/google-cloud.zsh
@@ -0,0 +1,6 @@
+#######################################################
+# google cloud
+# (google-cloud-sdk is installed at /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk. Add your profile:)
+#######################################################
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
